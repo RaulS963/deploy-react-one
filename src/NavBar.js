af5e1cd610349c5d6e76b4a1eea2716431f2e4e6
Anindya Das <dasanindya9809@gmail.com>
2020-03-23 00:41:06 +0530

    sdfghjkl

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,10 @@ function NavBar(props){
     return(
         <div className="navbar">
             <ul>
-                <li><a href="/" >Home</a></li>
-                <li><a href="/erza" >erza</a></li>
-                <li><a href="/mira" >mira</a></li>
-                <li><a href="/minerva" >minerva</a></li>
+                <li><a href="/#/" >Home</a></li>
+                <li><a href="/#/erza" >erza</a></li>
+                <li><a href="/#/mira" >mira</a></li>
+                <li><a href="/#/minerva" >minerva</a></li>
             </ul>
         </div>
     );
